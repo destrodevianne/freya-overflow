e885e20da35ac905f078597d0de8a59925f603d4
mrzephyr <mrzephyr@9bd6368f-5f5a-4f14-acbe-fb9f91336a0c>
2013-01-21 07:46:47 +0000

    -Community Smart agregado, primer parte

--- L2_CT2.5/L2_Gameserver/java/com/l2jserver/gameserver/communitybbs/Manager/TopBBSManager.java
+++ L2_CT2.5/L2_Gameserver/java/com/l2jserver/gameserver/communitybbs/Manager/TopBBSManager.java
@@ -14,9 +14,19 @@
  */
 package com.l2jserver.gameserver.communitybbs.Manager;
 
+import java.io.File;
 import java.util.StringTokenizer;
 
+import com.l2jserver.Config;
+import com.l2jserver.gameserver.GameTimeController;
 import com.l2jserver.gameserver.cache.HtmCache;
+import com.l2jserver.gameserver.communitybbs.CastleStatus;
+import com.l2jserver.gameserver.communitybbs.ClanList;
+import com.l2jserver.gameserver.communitybbs.GrandBossList;
+import com.l2jserver.gameserver.communitybbs.HeroeList;
+import com.l2jserver.gameserver.communitybbs.RaidList;
+import com.l2jserver.gameserver.communitybbs.TopPlayers;
+import com.l2jserver.gameserver.model.L2World;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.network.serverpackets.ShowBoard;
 
@@ -30,41 +40,103 @@ public class TopBBSManager extends BaseBBSManager
 	@Override
 	public void parsecmd(String command, L2PcInstance activeChar)
 	{
-		if (command.equals("_bbstop"))
+		String path = "data/html/CommunityBoard/";
+        String filepath = "";
+        String content = "";
+              
+        if (command.equals("_bbstop") | command.equals("_bbshome"))
 		{
-			String content = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), "data/html/CommunityBoard/index.htm");
-			if (content == null)
-			{
-				content = "<html><body><br><br><center>404 :File not found: 'data/html/CommunityBoard/index.htm' </center></body></html>";
-			}
-			separateAndSend(content, activeChar);
-		}
-		else if (command.equals("_bbshome"))
-		{
-			String content = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), "data/html/CommunityBoard/index.htm");
-			if (content == null)
-			{
-				content = "<html><body><br><br><center>404 :File not found: 'data/html/CommunityBoard/index.htm' </center></body></html>";
-			}
+			filepath = path + "index.htm";
+            content = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), filepath);
 			separateAndSend(content, activeChar);
 		}
 		else if (command.startsWith("_bbstop;"))
 		{
 			StringTokenizer st = new StringTokenizer(command, ";");
 			st.nextToken();
-			int idp = Integer.parseInt(st.nextToken());
-			String content = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), "data/html/CommunityBoard/" + idp + ".htm");
-			if (content == null)
+			String file = st.nextToken();
+            filepath = path + file + ".htm";
+            File filecom = new File(filepath);
+                      
+            if (!(filecom.exists()))
 			{
-				content = "<html><body><br><br><center>404 :File not found: 'data/html/CommunityBoard/" + idp
-				+ ".htm' </center></body></html>";
+				content = "<html><body><br><br><center>The command " + command + " points to file(" + filepath + ") that NOT exists.</center></body></html>";
+                               separateAndSend(content, activeChar);
+                               return;
 			}
+                       content = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), filepath);
+                      
+                       if (content.isEmpty())
+                               content = "<html><body><br><br><center>Content Empty: The command " + command + " points to an invalid or empty html file(" + filepath + ").</center></body></html>";
+                      
+                       switch (file)
+                       {
+                               case "toppvp":
+                                       TopPlayers pvp = new TopPlayers(file);
+                                       content = content.replaceAll("%toppvp%", pvp.loadTopList());
+                                       break;
+                               case "toppk":
+                                       TopPlayers pk = new TopPlayers(file);
+                                       content = content.replaceAll("%toppk%", pk.loadTopList());
+                                       break;
+                               case "toprbrank":
+                                       TopPlayers raid = new TopPlayers(file);
+                                       content = content.replaceAll("%toprbrank%", raid.loadTopList());
+                                       break;
+                               case "topadena":
+                                       TopPlayers adena = new TopPlayers(file);
+                                       content = content.replaceAll("%topadena%", adena.loadTopList());
+                                       break;
+                               case "toponline":
+                                       TopPlayers online = new TopPlayers(file);
+                                       content = content.replaceAll("%toponline%", online.loadTopList());
+                                       break;
+                               case "heroes":
+                                       HeroeList hr = new HeroeList();
+                                       content = content.replaceAll("%heroelist%", hr.loadHeroeList());
+                                       break;
+                               case "castle":
+                                       CastleStatus status = new CastleStatus();
+                                       content = content.replaceAll("%castle%", status.loadCastleList());
+                                       break;
+                               case "boss":
+                                       GrandBossList gb = new GrandBossList();
+                                       content = content.replaceAll("%gboss%", gb.loadGrandBossList());
+                                       break;
+                               case "stats":
+                                       content = content.replace("%online%", Integer.toString(L2World.getInstance().getAllPlayersCount()));
+                                       content = content.replace("%servercapacity%", Integer.toString(Config.MAXIMUM_ONLINE_USERS));
+                                       content = content.replace("%serverruntime%", getServerRunTime());
+                                       if (Config.ALLOW_REAL_ONLINE_STATS)
+                                               content = content.replace("%serveronline%", getRealOnline());
+                                       else
+                                               content = content.replace("%serveronline%", "");
+                                       break;
+                               default:
+                                       break;
+                      
+                       }
+                       if (file.startsWith("clan"))
+                       {
+                               int cid = Integer.parseInt(file.substring(4));
+                               ClanList cl = new ClanList(cid);
+                               content = content.replaceAll("%clanlist%", cl.loadClanList());
+                       }
+                       if (file.startsWith("raid"))
+                       {
+                               String rfid = file.substring(4);
+                               RaidList rd = new RaidList(rfid);
+                               content = content.replaceAll("%raidlist%", rd.loadRaidList());
+                       }
+                       if (content.isEmpty())
+                       {
+                               content = "<html><body><br><br><center>404 :File not found or empty: " + filepath + " your command is " + command + "</center></body></html>";
+                       }			
 			separateAndSend(content, activeChar);
 		}
 		else
 		{
-			ShowBoard sb = new ShowBoard("<html><body><br><br><center>the command: " + command
-					+ " is not implemented yet</center><br><br></body></html>", "101");
+                       ShowBoard sb = new ShowBoard("<html><body><br><br><center>the command: " + command + " is not implemented yet</center><br><br></body></html>", "101");
 			activeChar.sendPacket(sb);
 			activeChar.sendPacket(new ShowBoard(null, "102"));
 			activeChar.sendPacket(new ShowBoard(null, "103"));
@@ -86,4 +158,23 @@ public class TopBBSManager extends BaseBBSManager
 	{
 		protected static final TopBBSManager _instance = new TopBBSManager();
 	}
+      
+       public String getServerRunTime()
+       {
+               int timeSeconds = (GameTimeController.getGameTicks() - 36000) / 10;
+               String timeResult = "";
+               if (timeSeconds >= 86400)
+                       timeResult = Integer.toString(timeSeconds / 86400) + " Days " + Integer.toString((timeSeconds % 86400) / 3600) + " hours";
+               else
+                       timeResult = Integer.toString(timeSeconds / 3600) + " Hours " + Integer.toString((timeSeconds % 3600) / 60) + " mins";
+               return timeResult;
+       }
+      
+       public String getRealOnline()
+       {
+               int counter = 0;
+               counter = L2World.getInstance().getAllPlayersCount();
+               String realOnline = "<tr><td fixwidth=11></td><td FIXWIDTH=280>Players Active</td><td FIXWIDTH=470><font color=26e600>" + counter + "</font></td></tr>" + "<tr><td fixwidth=11></td><td FIXWIDTH=280>Players Shops</td><td FIXWIDTH=470><font color=26e600>" + (L2World.getInstance().getAllPlayersCount() - counter) + "</font></td></tr>";
+               return realOnline;
+       }
 }
