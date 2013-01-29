4b9c29988c6be5db673136abb40ddb0b67db81e4
mrzephyr <mrzephyr@9bd6368f-5f5a-4f14-acbe-fb9f91336a0c>
2013-01-29 16:37:44 +0000

    -Npc Vitality + Cambio de nombre Shop Donador html y multisell

---
+++ L2_CT2.5/L2_Datapack/dist/game/data/scripts/custom/9998_NPCVitality/__init__.py
@@ -0,0 +1,98 @@
+import sys
+from com.l2jserver.gameserver.model.actor.instance import L2PcInstance
+from com.l2jserver.gameserver.model.quest          import State
+from com.l2jserver.gameserver.model.quest          import QuestState
+from com.l2jserver.gameserver.model.quest.jython   import QuestJython as JQuest
+ 
+
+qn = "9998_NPCVitality"
+ 
+
+NPC=[40007]
+EVENTCOIN = 9142
+QuestId = 9998
+QuestName = "NPCVitality"
+QuestDesc = "custom"
+InitialHtml = "start.htm"
+
+print "INFO Loaded: 9998_NPCVitality"
+ 
+
+class Quest (JQuest) :
+
+
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
+
+
+ def onEvent(self,event,st) :
+
+        htmltext = event
+        count = st.getQuestItemsCount(EVENTCOIN)
+        if count == 0 :
+                return "no.htm"
+        else :
+            st.getPlayer().setTarget(st.getPlayer())
+        if event == "1":
+            count = st.getQuestItemsCount(EVENTCOIN)
+            if count < 1 :
+                return "malo.htm"
+            else : 
+                 st.takeItems(EVENTCOIN,1)
+    	    st.getPlayer().setVitalityPoints(1999,True)
+            st.playSound("ItemSound.quest_finish")
+            st.setState(State.COMPLETED)
+            st.exitQuest(1)
+            return "up.htm"
+
+        if event == "2":
+            count = st.getQuestItemsCount(EVENTCOIN)
+            if count < 2 :
+                return "malo.htm"
+            else : 
+                 st.takeItems(EVENTCOIN,2)
+            st.getPlayer().setVitalityPoints(12999,True)
+            st.playSound("ItemSound.quest_finish")
+            st.setState(State.COMPLETED)
+            st.exitQuest(1)
+            return "up.htm"
+
+        if event == "3":
+            count = st.getQuestItemsCount(EVENTCOIN)
+            if count < 3 :
+                return "malo.htm"
+            else : 
+                 st.takeItems(EVENTCOIN,3)
+            st.getPlayer().setVitalityPoints(16999,True)
+            st.playSound("ItemSound.quest_finish")
+            st.setState(State.COMPLETED)
+            st.exitQuest(1)
+            return "up.htm"
+
+        if event == "4":
+            count = st.getQuestItemsCount(EVENTCOIN)
+            if count < 4 :
+                return "malo.htm"
+            else : 
+                 st.takeItems(EVENTCOIN,4)
+            st.getPlayer().setVitalityPoints(19999,True)
+            st.playSound("ItemSound.quest_finish")
+            st.setState(State.COMPLETED)
+            st.exitQuest(1)
+            return "up.htm"
+
+        if htmltext != event:
+             st.setState(State.COMPLETED)
+             st.exitQuest(1)
+             return htmltext
+
+ def onFirstTalk (self,npc,player):
+        st = player.getQuestState(qn)
+        if not st :
+            st = self.newQuestState(player)
+        return InitialHtml
+QUEST = Quest(QuestId,str(QuestId) + "_" + QuestName,QuestDesc)
+
+for npcId in NPC:
+ QUEST.addStartNpc(npcId)
+ QUEST.addFirstTalkId(npcId)
+ QUEST.addTalkId(npcId)
