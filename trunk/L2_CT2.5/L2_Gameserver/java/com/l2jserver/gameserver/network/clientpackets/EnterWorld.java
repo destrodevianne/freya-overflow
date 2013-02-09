66c3292aa932a0162dc84bbbc61cc79a24c234ca
mrzephyr <mrzephyr@9bd6368f-5f5a-4f14-acbe-fb9f91336a0c>
2013-02-09 06:16:33 +0000

    -V2.0

--- trunk/L2_CT2.5/L2_Gameserver/java/com/l2jserver/gameserver/network/clientpackets/EnterWorld.java
+++ trunk/L2_CT2.5/L2_Gameserver/java/com/l2jserver/gameserver/network/clientpackets/EnterWorld.java
@@ -387,7 +387,8 @@ public class EnterWorld extends L2GameClientPacket
 		sendPacket(SystemMessage.getSystemMessage(SystemMessageId.WELCOME_TO_LINEAGE));
 		
 		activeChar.sendMessage(getText("RXN0ZSBzZXJ2aWRvciBlc3RhIGNvZGlmaWNhZG8gZW4gYmFzZSBhIEwySiBzZXJ2ZXIgQ1QyLjU="));
-		activeChar.sendMessage(getText("VmVyc2lvbiBkZSBMaW5la2tpdDogMS4x"));
+		activeChar.sendMessage(getText("QWN0dWFsaXphY2lvbmVzIGhlY2hhcyBwb3IgT3ZlcmZsb3IgRFA="));
+		activeChar.sendMessage(getText("VmVyc2lvbiBkZSBMaW5la2tpdDogMi4w"));
 		
 		if (Config.DISPLAY_SERVER_VERSION)
 		{
